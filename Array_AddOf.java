2fd904f3b5a143150501475c981418c95e831a31
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-07 16:18:28 +0530

    Add files via upload

---
+++ Array_AddOf.java
@@ -0,0 +1,17 @@
+package List;
+import java.util.ArrayList;
+public class Array_AddOf 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer>a=new ArrayList<>(3);
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		System.out.println(a);
+		a.add(40);
+		System.out.println(a);
+
+	}
+
+}
