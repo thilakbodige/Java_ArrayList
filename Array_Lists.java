14521a13b4e895f1819971282d3bd74205a4e843
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-07 16:18:11 +0530

    Add files via upload

---
+++ Array_Lists.java
@@ -0,0 +1,11 @@
+package List;
+import java.util.ArrayList;
+public class Array_Lists
+{
+	public static void main(String[] args)
+{
+   ArrayList<Integer>a=new ArrayList<>(3);
+   System.out.println(a);
+	}
+
+}
