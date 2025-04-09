12e31f4bba64f4009693b1a5effdae5be6f214dd
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-09 13:20:44 +0530

    Add files via upload

---
+++ Stack_Peak.java
@@ -0,0 +1,21 @@
+package List;
+
+import java.util.Stack;
+
+public class Stack_Peak
+{
+	public static void main(String[] args)
+	{
+		Stack<Integer> s=new Stack<>();
+	    s.add(10);
+	    s.add(20);
+	    s.add(30);
+	    s.add(40);
+	    s.add(50);
+	    System.out.println(s);
+	    System.out.println(s.peek());
+	    System.out.println(s);
+	}
+
+}
+
